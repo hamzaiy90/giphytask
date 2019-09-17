462426d95c456d4e05cd72bce67a8e7a775aeb57
hamzaiy90 <hamzaiy90@hotmail.com>
2019-09-17 14:52:54 +0100

    created the displaySearch component which renders the searched endpoint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
+import DisplaySearch from './DisplaySearch';
 import './App.css';
 
 let apiKey = process.env.REACT_APP_MY_API_KEY;
+
+const randomColor = [
+  'rgb(199, 193, 40)',
+  'rgb(222, 63, 63)',
+  'rgba(190, 117, 240, 0.88)',
+  'rgb(207, 115, 49)',
+  'rgb(54, 153, 61)'
+];
 
 class App extends Component {
   constructor(props) {
@@ -28,14 +37,20 @@ class App extends Component {
     }));
   }
 
+  onInputChange = (e) => {
+    this.setState({
+      searchInput: e.target.value.toLowerCase()
+    })
+  }
+
   render() {
 
     console.log(this.state.searchData);
     if (this.state.show == 'search') {
       return (
         <div className="container">
-          <Menu searchApi={this.searchApi} />
-          <h1>should display searched data</h1>
+          <Menu searchApi={this.searchApi} onInputChange={this.onInputChange}/>
+          <DisplaySearch randomColor={randomColor} searchdata={this.state.searchData} />
         </div>
       )
     }
---
+++ src/DisplaySearch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DisplaySearch = ({ searchdata, randomColor }) => {
+  if (searchdata.data) {
+    const searchArray = searchdata.data.map((img, i) => {
+      return <img style={{border:'20px solid ' + randomColor[Math.floor(Math.random() * 5)]}} key={i} src={img.images.original.url} alt="" />
+    })
+    console.log('broo its working...', searchArray)
+    return (
+      <div className="main-content">
+        {searchArray}
+      </div>
+    )
+  }
+  return <p>Make a search :)</p>
+}
+
+export default DisplaySearch;
