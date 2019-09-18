2e6d0e595e6f30d57ce0467852828bc66efae5cb
hamzaiy90 <hamzaiy90@hotmail.com>
2019-09-18 11:12:04 +0100

    Updates

---
+++ precache-manifest.9b6e7996e881f9897e88a288213f21f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b8d059c8aeedfdde6b1e84643ef3b9",
+    "url": "/giphytask/index.html"
+  },
+  {
+    "revision": "580a15a36a369004c829",
+    "url": "/giphytask/static/css/main.1658cc24.chunk.css"
+  },
+  {
+    "revision": "aff81cf001e796268c34",
+    "url": "/giphytask/static/js/2.6d36ac37.chunk.js"
+  },
+  {
+    "revision": "580a15a36a369004c829",
+    "url": "/giphytask/static/js/main.4629e0e2.chunk.js"
+  },
+  {
+    "revision": "02c0e3b4628e59e1c110",
+    "url": "/giphytask/static/js/runtime~main.dc7af32e.js"
+  }
+]);
